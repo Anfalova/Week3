5070cc519ad72afd936760568e50a61cf0f582bd
Евгений Анфалов <anfalovd10@yandex.ru>
2016-10-20 23:34:12 +0500

    Initialization

---
+++ Initialization/Initialization/ProcessorBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    class ProcessorBuilder
+    {
+        public static ProcessorEnginer<TEngine> CreateEngine<TEngine>()
+        {
+            return new ProcessorEnginer<TEngine>();
+        }
+    }
+}
---
+++ Initialization/Initialization/ProcessorEnginer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    class ProcessorEnginer<TEngine>
+    {
+        public ProcessorEntition<TEngine, TEntity> For<TEntity>()
+        {
+            return new ProcessorEntition<TEngine, TEntity>();
+        }
+    }
+}
